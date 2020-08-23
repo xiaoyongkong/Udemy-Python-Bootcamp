01fb6e1f55e6086d5343844fea1ba2862982a4ac
xiaofinancier <xiaoykong06@gmail.com>
2020-08-23 11:08:01 -0300

    including class of section 3

---
+++ section3/class15/class15.py
@@ -0,0 +1,17 @@
+#Print with double quotes
+print("Hello World");
+
+#It can also be with single quotes
+print('Hello World');
+
+#Using scape
+print("Hello  \tWorld");
+
+#Breakling line
+print("Hello \nWorld");
+
+#Lenght of the string
+noSpaces = len("Hello");
+withSpaces = len("I am");
+
+print("Lenght of Hello -> " + str(noSpaces) + "\nLenght of I am ->" + str(withSpaces) );
